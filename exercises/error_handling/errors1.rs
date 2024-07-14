955a174363c9da364e6c6cdaf4dd37d4326f726e
maosm <17681926710@126.com>
2024-07-15 00:05:12 +0800

    commit answer

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -9,17 +9,24 @@
 // Execute `rustlings hint errors1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-pub fn generate_nametag_text(name: String) -> Option<String> {
+// pub fn generate_nametag_text(name: String) -> Option<String> {
+//     if name.is_empty() {
+//         // Empty names aren't allowed.
+//         None
+//     } else {
+//         Some(format!("Hi! My name is {}", name))
+//     }
+// }
+pub fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
-        // Empty names aren't allowed.
-        None
+        // 空名称是不允许的，所以我们返回 Err 表示错误
+        Err("`name` was empty; it must be nonempty.".to_string())
     } else {
-        Some(format!("Hi! My name is {}", name))
+        // 名称不为空，我们创建一个问候语并返回 Ok(String)
+        Ok(format!("Hi! My name is {}", name))
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
